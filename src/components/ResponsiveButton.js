ccfc652f9c555c6ac240fb3f40572548544f1cc2
dennismorello <dennis.morello@contentwise.com>
2021-01-16 11:03:33 +0100

    fix(component): ref is now forwarded in ResponsiveButton

--- src/components/ResponsiveButton.js
+++ src/components/ResponsiveButton.js
@@ -1,13 +1,14 @@
-import { Button } from "@chakra-ui/react";
+import { Button, forwardRef } from "@chakra-ui/react";
 
-const ResponsiveButton = (props) => (
+const ResponsiveButton = forwardRef((props, forwardedRef) => (
   <Button
     fontSize={["md", "lg"]}
     h={[10, 12]}
     minW={[10, 12]}
     px={[4, 6]}
+    ref={forwardedRef}
     {...props}
   />
-);
+));
 
 export default ResponsiveButton;
